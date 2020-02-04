1e8832777df3f5b34b38949b96c349691ee74cb2
shanth kumar <shanth923@gmail.com>
2020-02-04 10:30:58 +0530

    Initial Commit

---
+++ UITask2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITask2
+//
+//  Created by Vidushi Duhan on 04/02/20.
+//  Copyright © 2020 Vidushi Duhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
